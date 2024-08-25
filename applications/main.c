91bcf3a47cb297df04d7cdc03bfe2cdfb1d1bf43
fusth3-master <2484874936@qq.com>
2024-08-26 01:42:11 +0800

    feat：开机显示进度条

--- applications/main.c
+++ applications/main.c
@@ -22,34 +22,36 @@ int main(void)
 //    flash_init();
     uart_init();
     led_init();
-    rt_uint8_t onchip_led_buf[8];
-    stm32_flash_read(ON_CHIP_FAL_OFFSET_ADDR, onchip_led_buf, 8);
-    if(onchip_led_buf[0] == 0xAA && onchip_led_buf[1] == 0x55)
-   {
-        uint8_t Calibration=0;
-       for(uint8_t i = 0; i < 7; i ++)
-       {
-           rt_kprintf("buffer[%d] = 0x%02x\n",i,onchip_led_buf[i]);
-           Calibration ^= onchip_led_buf[i];
-       }
-       rt_kprintf("buffer[%d] = 0x%02x\n",7,onchip_led_buf[7]);
-       if(Calibration == onchip_led_buf[+7])
-       {
-           g_rowled_data1_16.word32 = (rt_uint32_t)onchip_led_buf[2] \
-                                    | (rt_uint32_t)onchip_led_buf[3] << 8 \
-                                    | (rt_uint32_t)onchip_led_buf[4] << 16 \
-                                    | (rt_uint32_t)onchip_led_buf[5] << 24;
-           g_rowled_data17_18.word32 = (rt_uint32_t)onchip_led_buf[6];
-           rt_kprintf("Calibration success,g_rowled_data=0x%010X\n",g_rowled_data1_16.word32);
-       }
-   }
-    else
+    if( m5311_moudle_init(INIT_BLINK_LED) != RT_EOK)
     {
-        g_rowled_data1_16.word32 = 0xffffffff;
-        g_rowled_data17_18.word32 = 0x0000000f;
+        rt_uint8_t onchip_led_buf[8];
+        stm32_flash_read(ON_CHIP_FAL_OFFSET_ADDR, onchip_led_buf, 8);
+        if(onchip_led_buf[0] == 0xAA && onchip_led_buf[1] == 0x55)
+       {
+            uint8_t Calibration=0;
+           for(uint8_t i = 0; i < 7; i ++)
+           {
+               rt_kprintf("buffer[%d] = 0x%02x\n",i,onchip_led_buf[i]);
+               Calibration ^= onchip_led_buf[i];
+           }
+           rt_kprintf("buffer[%d] = 0x%02x\n",7,onchip_led_buf[7]);
+           if(Calibration == onchip_led_buf[+7])
+           {
+               g_rowled_data1_16.word32 = (rt_uint32_t)onchip_led_buf[2] \
+                                        | (rt_uint32_t)onchip_led_buf[3] << 8 \
+                                        | (rt_uint32_t)onchip_led_buf[4] << 16 \
+                                        | (rt_uint32_t)onchip_led_buf[5] << 24;
+               g_rowled_data17_18.word32 = (rt_uint32_t)onchip_led_buf[6];
+               rt_kprintf("Calibration success,g_rowled_data=0x%010X\n",g_rowled_data1_16.word32);
+           }
+       }
+        else
+        {
+            g_rowled_data1_16.word32 = 0xffffffff;
+            g_rowled_data17_18.word32 = 0x0000000f;
+        }
+        set_led();
     }
-    set_led();
-    m5311_moudle_init(REINIT_BLINK_LED);
     int count=0;
     for(;;)
     {
